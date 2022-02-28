284bdfba459e246dd8bfdb62c9c1b244d2ddfcbf
alenunez097 <alenunez097@gmail.com>
2022-02-27 23:49:04 -0500

    Se creo el index

--- libreria/libros/views.py
+++ libreria/libros/views.py
@@ -10,7 +10,7 @@ from .forms import bookForm
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('libros/index.html')
+    template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context,request))
 
